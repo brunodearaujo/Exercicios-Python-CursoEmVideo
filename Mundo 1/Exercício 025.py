1dc84d9e40f57fd4ac6f55965f94c35ebb17692f
Bruno de Araujo <hunterxxhunterba@gmail.com>
2020-04-27 23:55:32 -0300

    Resolvendo problemas de digitação. ")"

--- Mundo 1/Exercício 025.py
+++ Mundo 1/Exercício 025.py
@@ -1,4 +1,4 @@
 #Exercício Python 025: Crie um programa que leia o nome de uma pessoa e diga se ela tem "SILVA" no nome.
 nome = str(input('Digite o seu nome completo: ')).strip().lower()
-silva = bool(nome.split().count('silva')
+silva = bool(nome.split().count('silva'))
 print(f'O seu nome tem "Silva" ? {silva}')
